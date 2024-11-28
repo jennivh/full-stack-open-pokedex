12f0843853782e5e84ab448cdbe936e5927628d6
jennivh <jvharakka@gmail.com>
2024-11-28 13:56:54 +0200

    ?

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3000/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -75,9 +75,7 @@ module.exports = defineConfig({
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
-  use: {
-    baseURL: 'http://localhost:3000/',
-  },
+
 }
   /* Run your local dev server before starting the tests */
   //webServer: {
